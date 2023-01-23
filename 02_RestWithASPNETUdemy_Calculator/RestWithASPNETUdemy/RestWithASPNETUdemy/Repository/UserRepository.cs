6e0525477af5ec8b6ec838b4550ce7a9aa294007
Davisralves <davialves324@hotmail.com>
2023-01-23 17:54:30 -0300

    implementando o tokenService

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
@@ -2,6 +2,7 @@
 using RestWithASPNETUdemy.Model;
 using RestWithASPNETUdemy.Model.Context;
 using System;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -21,6 +22,26 @@ namespace RestWithASPNETUdemy.Repository
         {
             var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
             return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && u.Password == pass);
+        }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (_context.Users.Any(u => u.Id.Equals(user.Id))) return null;
+            var result = _context.Users.SingleOrDefault(u => u.Id.Equals(user.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return result;
         }
 
         private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
---
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implemetations/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RestWithASPNETUdemy.Configurations;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
+
+namespace RestWithASPNETUdemy.Services.Implemetations
+{
+    public class TokenService : ITokenService
+    {
+
+        private TokenConfiguration _configuration;
+
+        public TokenService(TokenConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAcessToken(IEnumerable<Claim> claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret));
+            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var options = new JwtSecurityToken(
+             issuer: _configuration.Issuer,
+             audience: _configuration.Audience,
+             claims: claims,
+             expires: DateTime.Now.AddMinutes(_configuration.Minutes),
+             signingCredentials: signingCredentials
+             );
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(options);
+            return tokenString;
+        }
+        public string GenerateRefreshToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
