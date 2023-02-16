4f83c51017f9213f302932d269b4c1e9d4c959c0
Davisralves <davialves324@hotmail.com>
2023-02-16 10:56:12 -0300

    Alterando acento errado no titulo da api

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -118,7 +118,7 @@ namespace RestWithASPNETUdemy
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Title = "REST API´s From 0 to Azure with ASP.NET Core 5 and Docker",
+                    Title = "REST API's From 0 to Azure with ASP.NET Core 5 and Docker",
                     Version = "v1",
                     Description = "Api RESTfull developed in REST API´s From 0 to Azure with ASP.NET Core 5 and Docker",
                     Contact = new OpenApiContact
